4d5067ad3803d960ed861922c78124a771947970
Sofiia <sonyprh13.05@gmail.com>
2025-02-25 17:01:31 +0100

    add requirements

--- Ukraine_map/Ukraine_map/settings.py
+++ Ukraine_map/Ukraine_map/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-x(1&=ch%gl*ec@z15=*4tf-)@!eseebxk#!td5p0se6hkf^xxs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
